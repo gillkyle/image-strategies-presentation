5f3a9f66b87c462bed1ad243525d646e87780891
Kyle Gill <kylerobertgill@gmail.com>
2018-03-28 21:13:34 -0600

    removing images, fixing typos

--- src/components/contact-info/index.js
+++ src/components/contact-info/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { FaPhone } from "react-icons/lib/fa";
+import React from 'react';
+import { FaPhone } from 'react-icons/lib/fa';
 
-import "./_contact-info.scss";
+import './_contact-info.scss';
 
 const ContactInfo = props => (
   <div className="contact-content">
-    <div>IS Group 12 - 1</div>
+    <div>IS Group 1 - 12</div>
     <div>Kyle Gill, Mckay Palmer, Matthew Hamilton, James Summers</div>
   </div>
 );
